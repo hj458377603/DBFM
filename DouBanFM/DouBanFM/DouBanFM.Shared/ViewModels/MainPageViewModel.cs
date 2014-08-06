3c682a2d05240cf5eeb241d9bf00852840149b61
potatoFM <458377603@qq.com>
2014-08-06 18:13:23 +0800

    新建项目--sk

---
+++ DouBanFM/DouBanFM/DouBanFM.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Caliburn.Micro;
+
+namespace DouBanFM
+{
+    public class MainPageViewModel : Screen 
+    {
+        readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        private string songName;
+        public string SongName 
+        {
+            get 
+            {
+                return songName;
+            }
+            set 
+            {
+                songName = value;
+                NotifyOfPropertyChange(() =>SongName);
+            }
+        }
+
+        public void Paly() 
+        {
+            SongName = "似的";
+        }
+    }
+}
